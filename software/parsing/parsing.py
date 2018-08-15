aed95989a5d11548a6b9b69703656570db3c6eaf
Hugo Ledoux <h.ledoux@tudelft.nl>
2018-08-15 10:00:22 +0200

    fix bug in parsing for adjacency

--- software/parsing/parsing.py
+++ software/parsing/parsing.py
@@ -12,25 +12,12 @@ def main():
     print ("Number CellSpace:", len(j['PrimalSpaceFeatures']))
     print ("Number dual graphs (SpaceLayers):", len(j['SpaceLayers']))
 
-    #-- are Cell C1 and C8 adjacent? either edge C1->C8 or C8->C1
-    c1 = j['PrimalSpaceFeatures']['C1']
-    c8 = j['PrimalSpaceFeatures']['C8']
-    # find the dual node
-    dual_c1 = j['SpaceLayers'][c1['duality-spacelayer']][c1['duality']]
-    dual_c8 = j['SpaceLayers'][c8['duality-spacelayer']][c8['duality']]
-    bFound = False
-    # iterate over the edges to see whether C8 is in C1
-    for e in dual_c1['edges']:
-        if (e['destination'] == 'C8'):
-            bFound = True
-            break
-    if (bFound == False):
-        # iterate over the edges to see whether C1 is in C8
-        for e in dual_c8['edges']:
-            if (e['destination'] == 'C1'):
-                bFound = True
-                break
-    print ("C1 and C8 adjacent?", bFound)
+    #-- are Cell C1 and C8 adjacent? [either edge C1->C8 or C8->C1]
+    print ("C1 and C8 adjacent?", are_cells_adjacent('C1', 'C8', j))
+    #-- are Cell C1 and C8 adjacent? 
+    print ("C1 and C3 adjacent?", are_cells_adjacent('C1', 'C3', j))
+    #-- are Cell C1 and C9 adjacent? 
+    print ("C1 and C9 adjacent?", are_cells_adjacent('C1', 'C9', j))
 
 
     #-- what is the dual of Node R10?
@@ -39,6 +26,31 @@ def main():
         if ('R10' in j['SpaceLayers'][graphid]):
             dual_r10 = j['SpaceLayers'][graphid]['R10']['duality']
     print ('Dual of R10:', str(dual_r10))
+
+
+def are_cells_adjacent(a, b, j):
+    if a not in j['PrimalSpaceFeatures']:
+        return False
+    if b not in j['PrimalSpaceFeatures']:
+        return False
+    ca = j['PrimalSpaceFeatures'][a]
+    cb = j['PrimalSpaceFeatures'][b]
+    # find the dual node
+    dual_ca = j['SpaceLayers'][ca['duality-spacelayer']][ca['duality']]
+    dual_cb = j['SpaceLayers'][cb['duality-spacelayer']][cb['duality']]
+    bFound = False
+    # iterate over the edges to see whether b is in a
+    for e in dual_ca['edges']:
+        if (e['destination'] == cb['duality']):
+            bFound = True
+            break
+    if (bFound == False):
+        # iterate over the edges to see whether a is in b
+        for e in dual_cb['edges']:
+            if (e['destination'] == ca['duality']):
+                bFound = True
+                break
+    return bFound
 
 
 if __name__ == '__main__':
